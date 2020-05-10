a0f689604a067fed2934fb4630b03b5064823b3c
IseTheSal <illya.ageevich@gmail.com>
2020-05-11 01:35:23 +0300

    Edited: add(); remove(); New: remove2(); getAllStudents(); searchByLogin(); updtaNameById(); changePasswordById();

--- src/by/tms/service/StudentService.java
+++ src/by/tms/service/StudentService.java
@@ -4,86 +4,79 @@ import by.tms.domain.Student;
 import by.tms.storage.StudentStorage;
 
 public class StudentService {
-	private StudentStorage studentStorage = new StudentStorage();
+    private StudentStorage studentStorage = new StudentStorage();
 
-	public boolean add(String name,
-					   String login,
-					   String password,
-					   String faculty,
-					   String group) {
-		if (!(studentStorage.existLogin())) {
-			Student student = new Student(name, login, password, faculty, group);
-			student.setLogin(student.getLogin().toUpperCase());
-			studentStorage.save(student);
-			return true;
-		} else {
-			return false;
-		}
-	}
+    //updateStudentById()
+	//updatePasswordById()
+	//updateNameById (long id , String name);
+	//updateFacultyById (long id , String faculty)
+	//public void updateGroupById (long id , String groupa)
+	//boolean checkById(Long id);
+	//checkByLogin(String login)
+	//public boolean checkByStudent(Student student)
+	//String getNameById(Long id)
+    public boolean add(String name, String login, String password, String faculty, String group) {
+        if (!(studentStorage.checkByLogin())) {
+            Student student = new Student(name, login, password, faculty, group);
+            student.setLogin(student.getLogin().toUpperCase());
+            studentStorage.save(student);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean removeById(Student student, String id) { //По существующему логину
-		if (studentStorage.existId(student.getId())) {
-			studentStorage.removeById(id);
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean remove(Student student) {
+        if (studentStorage.checkByStudent(student)) {
+            studentStorage.remove(student);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean removeById(Student student, String id) { //По существующему логину
-		if (studentStorage.existId(student.getId())) {
-			studentStorage.removeById(id);
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean remove2(String login) {
+        if (studentStorage.checkByLogin(login)) {
+            studentStorage.remove2(login);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public Student searchByLogin(String login) {
-		if (studentStorage.existLogin(login)) {
-			return studentStorage.getStudent();
-		} else {
-		}
-		return null;
-	}
+    public boolean getAllStudents() {
+        if (!(studentStorage.isEmpty())) {
+            studentStorage.printAll();
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean changePassword(Student student) {
-		if (studentStorage.existLogin(student.getLogin())) {
-			studentStorage.changePassword(student.getLogin()); // if(oldpassword.equals(student.getPassword()))
-			return true;
-		}
-		return false;
-	}
+    public Student searchByLogin(String login) {
+        if (studentStorage.checkByLogin(login)) {
+            return studentStorage.getByLogin(login);
+        } else {
+            return null;
+        }
+    }
 
-	public boolean getStudentGroupList(Student student) {
-		if (studentStorage.existGroup(student.getGroup())) {
-			studentStorage.getGroupList();
-			return true;
-		} else {
-			Writer.write("Группа не найдена");
-		}
-		return false;
-	}
+    public boolean updateNameById(long id, String name) {
+        if (studentStorage.checkByName(name) && studentStorage.checkById(id)) {
+            studentStorage.updateNameById(id, name);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean getFacultyList(String faculty) {
-		if (studentStorage.existFaculty(faculty)) {
-			studentStorage.getFacultyList();
-			return true;
-		} else {
-			Writer.write("Факультет не найден");
-		}
-		return false;
-	}
-
-	public boolean chageFaculty(Student student, String faculty) {
-		if (studentStorage.existFaculty(student.getFaculty())) {
-			studentStorage.changeFaculty;
-			return true;
-		}
-		else{
-			Writer.write("Такого факультета не существует");
-		}
-		return false;
-	}
+    public boolean changePasswordById(long id, String newPassword) {
+        if (studentStorage.checkById(id)) {
+            studentStorage.updatePasswordById(id, newPassword);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
