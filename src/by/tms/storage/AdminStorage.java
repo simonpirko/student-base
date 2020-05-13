b3b4c00a6ab0fe9f5f6c203740176a8bc5bb3c7a
Ivan Zubkov <you@example.com>
2020-05-13 21:23:57 +0300

    В AdminStorage методы: removeByLogin, removeById, updateNameByLogin, updateNameById, checkByLogin

--- src/by/tms/storage/AdminStorage.java
+++ src/by/tms/storage/AdminStorage.java
@@ -1,9 +1,6 @@
 package by.tms.storage;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AdminStorage {
     Connection connection = null;
@@ -12,7 +9,7 @@ public class AdminStorage {
 
         try {
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgress", "learn2000_");
-            PreparedStatement preparedStatement = connection.prepareStatement("update admins set id = default login = ?, password = ?, name = ?, role = ?");
+            PreparedStatement preparedStatement = connection.prepareStatement("update admins a set a.id = default a.login = ?, a.password = ?, name = ?, role = ?");
             preparedStatement.setString(1,login);
             preparedStatement.setString(2, password);
             preparedStatement.setString(3, name);
@@ -23,8 +20,76 @@ public class AdminStorage {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
+    public void removeByLogin (String login, String password) {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgress", "learn2000_");
+            PreparedStatement preparedStatement = connection.prepareStatement("delete * from admins a where a.login = ? and a.password = ?");
+            preparedStatement.setString(1,login);
+            preparedStatement.setString(2, password);
+            preparedStatement.executeQuery();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeById (long id, String password) {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgress", "learn2000_");
+            PreparedStatement preparedStatement = connection.prepareStatement("delete * from admins a where a.id = ? and a.password = ?");
+            preparedStatement.setLong(1,id);
+            preparedStatement.setString(2, password);
+            preparedStatement.executeQuery();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateNameByLogin (String login, String password, String name) {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgress", "learn2000_");
+            PreparedStatement preparedStatement = connection.prepareStatement("update admins a set a.name = ? where a.login = ? and a.password = ?");
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2,login);
+            preparedStatement.setString(3, password);
+            preparedStatement.executeQuery();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateNameById (long id, String password, String newName) {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgress", "learn2000_");
+            PreparedStatement preparedStatement = connection.prepareStatement("update admins a set a.name = ? where a.id = ? and a.password = ?");
+            preparedStatement.setString(1, newName);
+            preparedStatement.setLong(2, id);
+            preparedStatement.setString(3, password);
+            preparedStatement.executeQuery();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkByLogin (String login, String password) {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgress", "learn2000_");
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from admins a where a.login = ?");
+            preparedStatement.setString(1,login);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
