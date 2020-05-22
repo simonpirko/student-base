6d9b969b233d1c8be94b0351d973a5d4fad3d8dd
Ivan Zubkov <you@example.com>
2020-05-22 18:11:25 +0300

    Модифицированный метод saveAdmin и другие правки

---
+++ src/by/tms/domain/User.java
@@ -0,0 +1,9 @@
+package by.tms.domain;
+
+public class User extends UsersOfStudentBase {
+
+    public User(String name, String login, String password, String role) {
+        super(name, login, password, role);
+    }
+
+}
