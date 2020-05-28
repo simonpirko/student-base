37987a4388dc3dcdbd6265387f9538520574df23
Simon Pirko <sipi7gm@gmail.com>
2020-05-29 01:04:38 +0300

    Rev-5 Simple refactoring

--- src/by/tms/action/AdminAction.java
+++ src/by/tms/action/AdminAction.java
@@ -4,37 +4,37 @@ import by.tms.action.util.Writer;
 import by.tms.service.AdminService;
 
 import static by.tms.action.util.Reader.readId;
-import static by.tms.action.util.Reader.readWithInvite;
+//import static by.tms.action.util.Reader.readWithInvite;
 import static by.tms.action.util.Writer.writeln;
 
 public class AdminAction {
 
     private AdminService adminService = new AdminService();
 
-    public void addAdmin() {
-        String newAdminName = readWithInvite("Input administrator's Name: "),
-                newAdminLogin = readWithInvite("Input administrator's Login: "),
-                newAdminPassword = readWithInvite("Input administrator's Password: ");
-                String role = readWithInvite("Input Admin - 1 or Moderator - 2:");
-                switch (role) {
-                    case "1":
-                        if (adminService.addAdmin(newAdminName, newAdminLogin, newAdminPassword, Long.parseLong(role)));
-                        writeln("Administrator " + newAdminName + " was successfully added!");
-                    case "2":
-                        if (adminService.addAdmin(newAdminName, newAdminLogin, newAdminPassword, Long.parseLong(role)));
-                        writeln("Moderator " + newAdminName + " was successfully added!");
-                    default:
-                }
-        }
+//    public void addAdmin() {
+//        String newAdminName = readWithInvite("Input administrator's Name: "),
+//                newAdminLogin = readWithInvite("Input administrator's Login: "),
+//                newAdminPassword = readWithInvite("Input administrator's Password: ");
+//                String role = readWithInvite("Input Admin - 1 or Moderator - 2:");
+//                switch (role) {
+//                    case "1":
+//                        if (adminService.addAdmin(newAdminName, newAdminLogin, newAdminPassword, Long.parseLong(role)));
+//                        writeln("Administrator " + newAdminName + " was successfully added!");
+//                    case "2":
+//                        if (adminService.addAdmin(newAdminName, newAdminLogin, newAdminPassword, Long.parseLong(role)));
+//                        writeln("Moderator " + newAdminName + " was successfully added!");
+//                    default:
+//                }
+//        }
 
 
     public void removeAdmin() {
-        String login = readWithInvite("Input Administrator's Login to delete: "),
-                password = readWithInvite("Input Administrator's Password: ");
-        if (adminService.removeAdmin(login, password))
-            writeln("Student was successfully removed!");
-        else
-            writeln("Student with Login(" + login + ") not found OR password is incorrect!");
+//        String login = readWithInvite("Input Administrator's Login to delete: "),
+//                password = readWithInvite("Input Administrator's Password: ");
+//        if (adminService.removeAdmin(login, password))
+//            writeln("Student was successfully removed!");
+//        else
+//            writeln("Student with Login(" + login + ") not found OR password is incorrect!");
     }
 
     public boolean authorizationAdmin (String login, String password) {
@@ -48,30 +48,30 @@ public class AdminAction {
 
 
     public void findAdminByLogin() {
-        String adminLogin = readWithInvite("Input Administrator's Login: "), adminPassword = readWithInvite("Input Administrator's password: ");
-        if ((adminService.checkAdminByLogin(adminLogin, adminPassword)))
-            writeln("Founded Administrator:\n" + adminLogin);
-        else
-            writeln("Admin with Login(" + adminLogin + ") not found!");
+//        String adminLogin = readWithInvite("Input Administrator's Login: "), adminPassword = readWithInvite("Input Administrator's password: ");
+//        if ((adminService.checkAdminByLogin(adminLogin, adminPassword)))
+//            writeln("Founded Administrator:\n" + adminLogin);
+//        else
+//            writeln("Admin with Login(" + adminLogin + ") not found!");
     }
 
     public void findAdminById() {
         writeln("Input Administrator's Id: ");
         long adminId = readId();
-        String adminPassword = readWithInvite("Input Administrator's password: ");
-        if ((adminService.checkAdminById(adminId, adminPassword)))
-            writeln("Founded Administrator with Id: " + adminId);
-        else
-            writeln("Admin with Login(" + adminId + ") not found!");
+//        String adminPassword = readWithInvite("Input Administrator's password: ");
+//        if ((adminService.checkAdminById(adminId, adminPassword)))
+//            writeln("Founded Administrator with Id: " + adminId);
+//        else
+//            writeln("Admin with Login(" + adminId + ") not found!");
     }
 
     public void updateAdminNameById() {
         Writer.writeln("Input Administrator's Id:");
         Long adminId = readId();
-        String adminPassword = readWithInvite("Input Administrator's password:");
-        String adminNewName = readWithInvite("Input Administrator's new name:");
-        if (adminService.updateAdminNameById(adminId, adminPassword, adminNewName))
-            Writer.writeln("New name of Administrator with id " + adminId + " was set");
-        else writeln("Id " + adminId + " or password is incorrect!");
+//        String adminPassword = readWithInvite("Input Administrator's password:");
+//        String adminNewName = readWithInvite("Input Administrator's new name:");
+//        if (adminService.updateAdminNameById(adminId, adminPassword, adminNewName))
+//            Writer.writeln("New name of Administrator with id " + adminId + " was set");
+//        else writeln("Id " + adminId + " or password is incorrect!");
     }
 }
--- src/by/tms/action/ConsoleApplication.java
+++ src/by/tms/action/ConsoleApplication.java
@@ -1,162 +1,163 @@
 package by.tms.action;
 
-import static by.tms.action.util.Reader.*;
+import static by.tms.action.util.Reader.readMenuChoice;
 import static by.tms.action.util.Writer.writeln;
 
 public class ConsoleApplication {
 
-    StudentAction studentAction = new StudentAction();
-    AdminAction adminAction = new AdminAction();
+	StudentAction studentAction = new StudentAction();
+	AdminAction adminAction = new AdminAction();
 
-    public void run() {
+	public void run() {
+// TODO: 5/29/20 Impl menu
+	}
 
-    }
+	public void menu() {
+		int choice = 0;
+		String studentLogin;
+		String studentPassword;
+		String studentName;
+		String studentFaculty;
+		String studentGroup;
+		String adminLogin;
+		String adminPassword;
 
-    public void menu() {
-        int choice = 0;
-        String studentLogin;
-        String studentPassword;
-        String studentName;
-        String studentFaculty;
-        String studentGroup;
-        String adminLogin;
-        String adminPassword;
+		while (choice != 0) {
+			writeln("Меню:");
+			writeln("Авторизация - 1");
+			writeln("Регистрация - 2");
+			writeln("Выход - 0");
+			choice = readMenuChoice();
+			if ((choice == -1)) {
+				writeln("Введите корректное значение");
+				break;
+			} else {
+				switch (choice) {
+					case 1:
+						writeln("Авторизация студента - 1, Авторизация администратора - 2, Авторизация модератора - 3");
+						int authChoice = readMenuChoice();
+						switch (authChoice) {
+							case 1:
+//                                studentLogin = readWithInvite("Input student login");
+//                                studentPassword = readWithInvite("Input student password");
 
-        while (choice != 0) {
-            writeln("Меню:");
-            writeln("Авторизация - 1");
-            writeln("Регистрация - 2");
-            writeln("Выход - 0");
-            choice = readMenuChoice();
-            if ((choice == -1)) {
-                writeln("Введите корректное значение");
-                break;
-            } else {
-                switch (choice) {
-                    case 1:
-                        writeln("Авторизация студента - 1, Авторизация администратора - 2, Авторизация модератора - 3");
-                        int authChoice = readMenuChoice();
-                        switch (authChoice) {
-                            case 1:
-                                studentLogin = readWithInvite("Input student login");
-                                studentPassword = readWithInvite("Input student password");
+								// FIXME: 5/27/20
+//                                if (studentAction.authorizationStudent(studentLogin, studentPassword)) {
+//                                    casesForStudent();
+//                                    writeln("Auth ok!");
+//                                } else {
+//                                    writeln("Error in login or password!");
+								break;
+						}
 
-                                // FIXME: 5/27/20
-                                if (studentAction.authorizationStudent(studentLogin, studentPassword)) {
-                                    casesForStudent();
-                                    writeln("Auth ok!");
-                                } else {
-                                    writeln("Error in login or password!");
-                                    break;
-                                }
+					case 2:
+//                                adminLogin = readWithInvite("Input admin login");
+//                                adminPassword = readWithInvite("Input admin password");
+//                                if (adminAction.authorizationAdmin(adminLogin, adminPassword)) {
+//                                    casesForAdmin();
+//                                } else {
+//                                    writeln("Error in login or password!");
+						break;
+//                                }
+				}
+				case 2:
+					writeln("Регистрация студента - 1, Регистрация администратора - 2, Регистрация модератора - 3");
+					int regChoice = readMenuChoice();
+					switch (regChoice) {
+						case 1:
+//                                studentLogin = readWithInvite("Input student login");
+//                                studentPassword = readWithInvite("Input student password");
+//                                studentName = readWithInvite("Input name");
+//                                studentFaculty = readWithInvite("Input faculty");
+//                                studentGroup = readWithInvite("Input group");
 
-                            case 2:
-                                adminLogin = readWithInvite("Input admin login");
-                                adminPassword = readWithInvite("Input admin password");
-                                if (adminAction.authorizationAdmin(adminLogin, adminPassword)) {
-                                    casesForAdmin();
-                                } else {
-                                    writeln("Error in login or password!");
-                                    break;
-                                }
-                        }
-                    case 2:
-                        writeln("Регистрация студента - 1, Регистрация администратора - 2, Регистрация модератора - 3");
-                        int regChoice = readMenuChoice();
-                        switch (regChoice) {
-                            case 1:
-                                studentLogin = readWithInvite("Input student login");
-                                studentPassword = readWithInvite("Input student password");
-                                studentName = readWithInvite("Input name");
-                                studentFaculty = readWithInvite("Input faculty");
-                                studentGroup = readWithInvite("Input group");
-
-                                // FIXME: 5/27/20
-                                if (studentAction.registrationStudent(studentName, studentLogin, studentPassword, studentFaculty, studentGroup, 3)) {
-                                    writeln("Student + " + studentName + " added");
-                                    casesForStudent();
-                                    } else {
-                                    writeln("This login already exist. Try another.");
-                                    break;
-                                }
-                            case 2:
-                                adminLogin = readWithInvite("Input admin login");
-                                adminPassword = readWithInvite("Input admin password");
-                                if (adminAction.authorizationAdmin(adminLogin, adminPassword)) {
-                                    casesForAdmin();
-                                }
-                            case 0:
-                                break;
-                            default:
-                                writeln("Введите корректное значение");
-                        }
-                }
-            }
-        }
-    }
-
-    public void casesForStudent() {
-        int studentChoice = 0;
-        while (studentChoice != -1) {
-            writeln("Выберите действия для студентов:");
-            writeln("Изменить имя студента по Id - 1");
-            writeln("Изменить пароль студента по логину - 2");
-            writeln("Обратно в меню = 0");
-            studentChoice = readMenuChoice();
-            switch (studentChoice) {
-                case 1:
-                    studentAction.updateStudentNameById();
-                case 2:
-                    studentAction.changeStudentPasswordByLogin();
-                case 0:
-                    menu();
-                default:
-                    writeln("Некорректное значение");
-            }
-            studentChoice = -1;
-        }
-    }
-
-    public void casesForAdmin() {
-        int adminChoice = 0;
-        while (adminChoice != -1) {
-            writeln("Выберите действия для админов:");
-            writeln("Найти студента по логину - 1");
-            writeln("Изменить имя студента по Id- 2");
-            writeln("Изменить пароль студента по логину - 3");
-            writeln("Изменить название факультета - 4");
-            writeln("Изменить название группы - 5");
-            writeln("Вывести список всех студентов - 6");
-            writeln("Вывести список всех студентов группы - 7");
-            writeln("Вывести список все студентов факультета - 8");
-            writeln("Обновить название факультета по Id- 9");
-            writeln("Обратно в меню = 0");
-            adminChoice = readMenuChoice();
-            switch (adminChoice) {
-                case 1:
-                    studentAction.findByLogin();
-                case 2:
-                    studentAction.updateStudentNameById();
-                case 3:
-                    studentAction.updatePasswordById();
-                case 4:
-                    studentAction.updateFacultyById();
-                case 5:
-                    studentAction.updateGroupById();
-                case 6:
-                    studentAction.findAllStudents();
-                case 7:
-                    studentAction.getStudentGroupList();
-                case 8:
-                    studentAction.getStudentFacultyList();
-                case 9:
-                    studentAction.updateFacultyById();
-                case 0:
-                    menu();
-                default:
-                    writeln("Некорректное значение");
-            }
-            adminChoice = -1;
-        }
-    }
+							// FIXME: 5/27/20
+//                                if (studentAction.registrationStudent(studentName, studentLogin, studentPassword, studentFaculty, studentGroup, 3)) {
+//                                    writeln("Student + " + studentName + " added");
+//                                    casesForStudent();
+//                                    } else {
+//                                    writeln("This login already exist. Try another.");
+							break;
+//                                }
+						case 2:
+//                                adminLogin = readWithInvite("Input admin login");
+//                                adminPassword = readWithInvite("Input admin password");
+//                                if (adminAction.authorizationAdmin(adminLogin, adminPassword)) {
+//                                    casesForAdmin();
+//                                }
+						case 0:
+							break;
+						default:
+							writeln("Введите корректное значение");
+					}
+			}
+		}
+	}
 }
+    }
+
+public void casesForStudent(){
+		int studentChoice=0;
+		while(studentChoice!=-1){
+		writeln("Выберите действия для студентов:");
+		writeln("Изменить имя студента по Id - 1");
+		writeln("Изменить пароль студента по логину - 2");
+		writeln("Обратно в меню = 0");
+		studentChoice=readMenuChoice();
+		switch(studentChoice){
+		case 1:
+		studentAction.updateStudentNameById();
+		case 2:
+		studentAction.changeStudentPasswordByLogin();
+		case 0:
+		menu();
+default:
+		writeln("Некорректное значение");
+		}
+		studentChoice=-1;
+		}
+		}
+
+public void casesForAdmin(){
+		int adminChoice=0;
+		while(adminChoice!=-1){
+		writeln("Выберите действия для админов:");
+		writeln("Найти студента по логину - 1");
+		writeln("Изменить имя студента по Id- 2");
+		writeln("Изменить пароль студента по логину - 3");
+		writeln("Изменить название факультета - 4");
+		writeln("Изменить название группы - 5");
+		writeln("Вывести список всех студентов - 6");
+		writeln("Вывести список всех студентов группы - 7");
+		writeln("Вывести список все студентов факультета - 8");
+		writeln("Обновить название факультета по Id- 9");
+		writeln("Обратно в меню = 0");
+		adminChoice=readMenuChoice();
+		switch(adminChoice){
+		case 1:
+		studentAction.findByLogin();
+		case 2:
+		studentAction.updateStudentNameById();
+		case 3:
+		studentAction.updatePasswordById();
+		case 4:
+		studentAction.updateFacultyById();
+		case 5:
+		studentAction.updateGroupById();
+		case 6:
+		studentAction.findAllStudents();
+		case 7:
+		studentAction.getStudentGroupList();
+		case 8:
+		studentAction.getStudentFacultyList();
+		case 9:
+		studentAction.updateFacultyById();
+		case 0:
+		menu();
+default:
+		writeln("Некорректное значение");
+		}
+		adminChoice=-1;
+		}
+		}
+		}
